84fd37d2a657387957550e6cf20b1b926884cfde
Samtav007 <samtav0821@gmail.com>
2024-04-19 10:44:26 +0530

    done

--- src/components/PureCounterComponent.jsx
+++ src/components/PureCounterComponent.jsx
@@ -1,6 +1,6 @@
-import React, { Component } from 'react';
+import React, { PureComponent } from 'react';
 
-class Pure extends Component {
+class Pure extends PureComponent {
   constructor(props) {
     super(props);
     this.state = {
